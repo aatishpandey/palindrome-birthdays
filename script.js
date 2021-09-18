c66a44d50f6ca9c50a60a27218ce038bc0cb6fac
Aatish Pandey <32216307+aatishpandey@users.noreply.github.com>
2021-09-18 16:42:41 +0530

    update styles.css

--- script.js
+++ script.js
@@ -35,11 +35,7 @@ function convertDateToString(date){
     return dateStr;
 }
 
-var date = {
-    day:2,
-    month:11,
-    year:2020
-};
+
 
 function getAllDateFormats(date){
     var dateStr = convertDateToString(date);
@@ -68,4 +64,131 @@ function checkPalindromeForAllDateFormats(date){
     return PalindromeHai;
 }
 
-console.log(checkPalindromeForAllDateFormats(date));
+function isLeapYear(year){
+     if(year %400 === 0){
+        return true;
+     }
+     if(year%100 ===0){
+         return false;
+     }
+     if(year%4===0){
+         return true;
+     }
+     return false;
+}
+
+function getNextDate(date){
+    var day = date.day+1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth =[31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if(month === 2){
+        //check for leap year
+        if(isLeapYear(year)){
+            if(day>29){
+                day=1;
+                month++;
+            }
+        }
+        else{
+            if(day>28){
+                day=1;
+                month++;
+            }
+        }
+    }
+    else{
+        if(day > daysInMonth[month - 1]){ //agar 31 day dia or month 04(4-1 =3 array mai) ,to day=1 ho jyega ot month change hokar next i.e 05(5-1 in array)
+            day = 1;
+            month++;
+        }
+    }
+
+    if(month>12){
+        month=1;
+        year++;
+    }
+
+    return {
+        day : day,
+        month:month,
+        year:year
+    }
+}
+
+function getNextPalindromeDate(date){
+    var ctr=0;
+    var nextDate = getNextDate(date);
+
+    while(1){
+        ctr++;
+        var PalindromeHaiKya = checkPalindromeForAllDateFormats(nextDate);
+        if(PalindromeHaiKya){
+            break;
+        }
+        nextDate = getNextDate(nextDate);
+    }
+
+    return[ctr,nextDate];
+}
+
+function getPreviousDate(date){
+    day = date.day - 1;
+    month = date.month;
+    year = date.year;
+
+    var daysInMonth =[31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if(day === 0){
+        month--;
+    }
+
+    if(month === 0){
+        month = 12;
+        year--;
+        day=31;
+    }else if(month === 2){
+        if(isLeapYear){
+          day=29;
+        }else{
+            day = 28;
+        }
+    }else{
+        day = daysInMonth[month - 1];
+    }
+
+    return{
+        day:day,
+        month:month,
+        year:year
+    }
+
+}
+
+function getPreviousPalindromeDate(date){
+    ctr=0;
+    getPrevDate = getPreviousDate(date);
+
+    while(1){
+        ctr++;
+        var PalindromeHaiKya = checkPalindromeForAllDateFormats(getPrevDate);
+        if(PalindromeHaiKya){
+            break;
+        }else{
+            getPrevDate = getPreviousDate(getPrevDate);
+        }
+
+        return[ctr,getPrevDate];
+    }
+}
+
+var date = {
+    day:3,
+    month:11,
+    year:2020
+};
+
+
+console.log(getPreviousPalindromeDate(date));
